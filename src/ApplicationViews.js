0eabcf82a3727d709c9c7b6ca2ef91fa7ad784fb
Cherkesky <guy@cherkesky.com>
2019-12-10 13:56:22 -0600

    login and register routing

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Route } from "react-router-dom";
 import Welcome from "./components/welcome/Welcome"
+import Register from "./components/register/Register"
+import Login from "./components/login/Login"
+
 
 export class ApplicationViews extends Component {
   render() {
@@ -11,7 +14,20 @@ export class ApplicationViews extends Component {
             return <Welcome {...props} />
           }}
         />
+
+        <Route
+          exact path="/register" render={props => {
+            return <Register {...props} />
+          }}
+        />
+
+        <Route
+          exact path="/login" render={props => {
+            return <Login {...props} />
+          }}
+        />
       </React.Fragment>
+
     )
   }
 }
--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Register from "../register/Register"
 import Logo from '../../assets/ChaserLogo.png'
 
 export class Welcome extends Component {
@@ -9,8 +10,12 @@ export class Welcome extends Component {
         <img src={Logo} alt={"logo"}/>  
 
         <div>
-        <button type="button" id="login" >Login</button>
-        <button type="button" id="signup" >Sign Up</button><br/>
+        <button type="button" id="login" onClick={()=>{
+         this.props.history.push("/login")
+          }}>Login</button>
+        <button type="button" id="signup" onClick={()=>{
+         this.props.history.push("/register")
+          }} >Sign Up</button><br/>
         </div>
         
 
