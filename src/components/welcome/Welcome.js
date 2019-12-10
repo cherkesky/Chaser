d7d475a672cae984c63ac584fde12b43d73d6fd9
Cherkesky <guy@cherkesky.com>
2019-12-10 14:07:13 -0600

    materialUI installed and replaced the welcome screen buttons

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,23 +1,27 @@
 import React, { Component } from 'react'
-import Register from "../register/Register"
 import Logo from '../../assets/ChaserLogo.png'
+import Button from '@material-ui/core/Button';
 
 export class Welcome extends Component {
   render() {
     return (
       <div>
 
-        <img src={Logo} alt={"logo"}/>  
+        <img src={Logo} alt={"logo"} />
 
         <div>
-        <button type="button" id="login" onClick={()=>{
-         this.props.history.push("/login")
-          }}>Login</button>
-        <button type="button" id="signup" onClick={()=>{
-         this.props.history.push("/register")
-          }} >Sign Up</button><br/>
+          <Button variant="contained" color="secondary"onClick={() => {
+            this.props.history.push("/login")
+          }} >
+            Login
+          </Button>
+          <Button variant="contained" color="default"onClick={() => {
+            this.props.history.push("/register")
+          }}>
+           Sign Up
+          </Button>
         </div>
-        
+
 
       </div>
     )
