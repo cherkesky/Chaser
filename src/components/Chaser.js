cdec9eacca3ef706ff1d525c2ac7234ef139fc25
Cherkesky <guy@cherkesky.com>
2019-12-10 10:37:33 -0600

    initial file structure

---
+++ src/components/Chaser.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Chaser extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Chaser Component</h1>
+      </div>
+    )
+  }
+}
+
+export default Chaser
