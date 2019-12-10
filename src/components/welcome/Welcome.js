587ab9fc4220004d6c171cd7323339352bdef279
Cherkesky <guy@cherkesky.com>
2019-12-10 13:31:48 -0600

    logo

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
+import Logo from '../../assets/ChaserLogo.png'
 
 export class Welcome extends Component {
   render() {
     return (
-      <div class="container">
+      <div>
+
+        <img src={Logo} alt={"logo"}/>  
 
         <div>
-          <h2>Background Image</h2>
-        </div>
-
         <button type="button" id="login" >Login</button>
-        <button type="button" id="signup" >Sign Up</button>
-
+        <button type="button" id="signup" >Sign Up</button><br/>
+        </div>
         
 
       </div>
