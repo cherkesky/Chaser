3c57da256ba9271fd4155e707cf75593f9688752
Cherkesky <guy@cherkesky.com>
2019-12-10 21:33:24 -0600

    navbar shows only if logged in

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import Pic from "../../assets/user1.png"
+
+const useStyles = theme => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  bigAvatar: {
+    width: 60,
+    height: 60,
+  },
+    });
+
 export class Navbar extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <div>
-        <h1>NavBar</h1>
-      </div>
+      <>
+        {
+           (this.props.user) 
+           ? <div className={classes.root}>
+           <Avatar alt="Avatar" src={Pic}
+            className={classes.bigAvatar} />
+         </div>
+           : null
+        }
+      </>
     )
   }
 }
+export default withStyles(useStyles)(Navbar)
 
-export default Navbar
+// export default Navbar
