3aeb952231031ec3625a5b493fc5f122be3a78a0
Cherkesky <guy@cherkesky.com>
2019-12-11 15:20:55 -0600

    added some basic login to the register form. still problems with the radio buttons

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import ApiManager from '../../modules/ApiManager';
 import { withStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 
 const useStyles = theme => ({
   root: {
@@ -20,36 +22,48 @@ export class Navbar extends Component {
   state = {
     users: {}
   }
-  loggedInUserId() {return parseInt(localStorage.getItem("userId"))}
-
-  componentDidMount(){
+  loggedInUserId() { return parseInt(localStorage.getItem("userId")) }
+  //*****************************************************************************************************
+  // componentDidMount()
+  //*****************************************************************************************************
+  componentDidMount() {
     ApiManager.get("users", this.loggedInUserId())
       .then((usersArr) => {
-         this.setState(
-           {
-             users: usersArr
-           }
-         )
-       })
+        this.setState(
+          {
+            users: usersArr
+          }
+        )
+      })
   }
 
+  //*****************************************************************************************************
+  // Render()
+  //*****************************************************************************************************
 
   render() {
-    const { classes } = this.props; 
+    const { classes } = this.props; // material ui styling dependency
 
-    const profileImageUrl = this.state.users.avatarUrl ? this.state.users.avatarUrl : "noimage.png" 
+    const profileImageUrl = this.state.users.avatarUrl ? this.state.users.avatarUrl : "noimage.png"
     console.log(this.state)
-     return (
+    return (
       <>
         {
-          (this.props.user)
-            ? <div className={classes.root}>
-              <Avatar alt="Avatar" src={require(`./../../assets/${profileImageUrl}`)} variant="circle"
-                className={classes.bigAvatar}
-                onClick={()=> {window.alert("AVATAR CLICKED")}}
-                 />
-            </div>
-            : null
+          (this.props.user) // have anyone logged in?
+            ? // yes? good. show the nav bar
+            <AppBar position="static" style={{ background: 'transparent'}}>
+              <Toolbar>
+              <div className={classes.root}>
+                <Avatar alt="Avatar" src={require(`./../../assets/${profileImageUrl}`)} variant="circle"
+                  className={classes.bigAvatar}
+                  onClick={() => { window.alert("AVATAR CLICKED") }}
+                />
+                <img alt="logo" src={require("../../assets/ChaserLogo.png")} width="200px" height="40px" onClick={() => { window.alert("LOGO CLICKED") }}></img> 
+                <img alt="gears" src={require("../../assets/gears.svg")} width="20px" height="20px" onClick={() => { window.alert("PREFERENCES CLICKED") }}></img>
+              </div>
+              </Toolbar>
+            </AppBar>
+            : null // no? dont show the nav bar
         }
       </>
     )
--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ApiManager from '../../modules/ApiManager';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -10,91 +11,172 @@ import Button from '@material-ui/core/Button';
 
 
 export class Register extends Component {
-  
-  
+
+  state = {
+    firstName: '',
+    lastName: '',
+    age: '',
+    gender: '',
+    genderInterested: '',
+    email: '',
+    password: '',
+    passwordB: '',
+    buttonDisabled: true
+  };
+ //*****************************************************************************************************
+  // Handle Field Change
+  //*****************************************************************************************************
+  handleFieldChange = e => {
+    const stateToChange = {}
+    stateToChange[e.target.id] = e.target.value
+    this.setState(stateToChange)
+  };
+
+  //*****************************************************************************************************
+  // Handle Register
+  //*****************************************************************************************************
+  handleRegister = e => {
+    e.preventDefault()
+    const { passwordA, passwordB } = this.state
+    if (passwordA === passwordB && passwordA !== "") {
+      const newUser = {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        age: this.state.age,
+        gender: this.state.gender,
+        genderInterested: this.state.genderInterested,
+        email: this.state.email,
+        password: this.state.passwordA,
+      }
+      this.props.setUser({
+        email: this.state.email,
+        password: this.state.passwordA,
+      })
+      ApiManager.post("users", newUser)
+        .then(() => {
+          ApiManager.getLoggedInuser(this.state.email)
+            .then((user) => {
+              // console.log('user registration', user)
+              const userId = user[0].id
+              localStorage.setItem("userId", parseInt(userId))
+            })
+        })
+        .then(() => {
+          this.props.history.push("/")
+        })
+    }
+    else {
+      window.alert("Please make sure your passwords match")
+    }
+  }
+
+  //*****************************************************************************************************
+  // Render()
+  //*****************************************************************************************************
+
   render() {
+
+    const { email, password, passwordB } = this.state;
+    const isEnabled = email.length > 0 && password.length > 0 && passwordB.length > 0 && password === passwordB;
+
     return (
       <div>
         <FormControl component="fieldset">
           <FormGroup>
-
             <TextField required
-              id="first"
+              id="firstName"
               label="First Name"
+              onChange={this.handleFieldChange}
             /><br />
             <TextField required
-              id="last"
+              id="lastName"
               label="Last Name"
+              onChange={this.handleFieldChange}
             /><br />
             <TextField required
               id="age"
               label="Age"
               type="number"
+              onChange={this.handleFieldChange}
             /><br />
-<FormControl component="fieldset">
-      <FormLabel component="legend">Gender</FormLabel>
-      <RadioGroup aria-label="position" name="position" row>
-        <FormControlLabel
-          value="male"
-          control={<Radio color="primary" />}
-          label="Male"
-          labelPlacement="bottom"
-        />
-        <FormControlLabel
-          value="female"
-          control={<Radio color="secondary" />}
-          label="Female"
-          labelPlacement="bottom"
-        />
-        <FormControlLabel
-          value="other"
-          control={<Radio color="default" />}
-          label="Other"
-          labelPlacement="bottom"
-        />
-      </RadioGroup>
-    </FormControl>
-    <FormControl component="fieldset">
-      <FormLabel component="legend">Gender interested in</FormLabel>
-      <RadioGroup aria-label="position" name="position" row>
-        <FormControlLabel
-          value="male"
-          control={<Radio color="primary" />}
-          label="Male"
-          labelPlacement="bottom"
-        />
-        <FormControlLabel
-          value="female"
-          control={<Radio color="secondary" />}
-          label="Female"
-          labelPlacement="bottom"
-        />
-        <FormControlLabel
-          value="other"
-          control={<Radio color="default" />}
-          label="Other"
-          labelPlacement="bottom"
-        />
-      </RadioGroup>
-    </FormControl>
+            <FormControl component="fieldset" id="gender">
+              <FormLabel component="legend">Gender</FormLabel>
+              <RadioGroup
+                id="gender"
+                aria-label="position"
+                name="gender"
+                onChange={this.handleFieldChange}
+                row>
+                <FormControlLabel
+                  value="male"
+                  control={<Radio color="primary" />}
+                  label="Male"
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="female"
+                  control={<Radio color="secondary" />}
+                  label="Female"
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="other"
+                  control={<Radio color="default" />}
+                  label="Other"
+                  labelPlacement="bottom"
+                />
+              </RadioGroup>
+            </FormControl>
+            <FormControl component="fieldset" id="genderInterested">
+              <FormLabel component="legend">Gender interested in</FormLabel>
+              <RadioGroup
+                id="genderInterested"
+                aria-label="position"
+                name="genderInterested"
+                onChange={this.handleFieldChange}
+                row>
+                <FormControlLabel
+                  value="male"
+                  control={<Radio color="primary" />}
+                  label="Male"
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="female"
+                  control={<Radio color="secondary" />}
+                  label="Female"
+                  labelPlacement="bottom"
+                />
+                <FormControlLabel
+                  value="other"
+                  control={<Radio color="default" />}
+                  label="Other"
+                  labelPlacement="bottom"
+                />
+
+              </RadioGroup>
+            </FormControl>
             <TextField required
               id="email"
               label="Email"
               type="email required"
+              onChange={this.handleFieldChange}
             /><br />
             <TextField required
               id="password"
               label="Password"
               type="password"
+              onChange={this.handleFieldChange}
             /><br />
             <TextField required
-              id="password-again"
+              id="passwordB"
               label="Password again"
               type="password"
+              onChange={this.handleFieldChange}
             /><br />
-            <Button variant="contained" color="secondary" onClick={() => {
-            this.props.history.push("/checkin")
-          }}>
+            <Button disabled={!isEnabled} variant="contained" color="secondary" onClick={() => {
+              this.props.history.push("/checkin") // <-----------------------change to handleRegister
+            }}>
               Sign Up
           </Button>
           </FormGroup>
