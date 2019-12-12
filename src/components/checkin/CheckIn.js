cdfee9c2e8a6c1834fcdbdea61470d3f7c9c9902
Cherkesky <guy@cherkesky.com>
2019-12-12 14:17:17 -0600

    reading my geolocation

--- src/components/checkin/CheckIn.js
+++ src/components/checkin/CheckIn.js
@@ -1,6 +1,42 @@
 import React, { Component } from 'react'
 
 export class CheckIn extends Component {
+  //*****************************************************************************************************
+  // ComponentDidMount()
+  //*****************************************************************************************************
+  componentDidMount(){
+    const getLocationX=()=> {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((showPositionX));
+      } else { 
+        console.log ("Geolocation is not supported by this browser.")
+      }
+    }
+    function showPositionX(position) {
+      console.log("Latitude: ", position.coords.latitude) 
+    }
+    const getLocationY=()=> {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((showPositionY));
+      } else { 
+        console.log ("Geolocation is not supported by this browser.")
+      }
+    }
+    function showPositionY(position) {
+      console.log("Longitude: ", position.coords.longitude) 
+    }
+
+    getLocationX()
+    getLocationY()
+  }
+
+
+
+
+
+  //*****************************************************************************************************
+  // Render()
+  //*****************************************************************************************************
   render() {
     return (
       <div>
