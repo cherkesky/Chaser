b191eb8343552af4797537b03c57259495c9596a
Cherkesky <guy@cherkesky.com>
2019-12-14 08:58:15 -0600

    fixed senddrink button icon

--- src/components/drinks/SendDrink.js
+++ src/components/drinks/SendDrink.js
@@ -7,29 +7,29 @@ import LocalBarOutlinedIcon from '@material-ui/icons/LocalBarOutlined';
 
 
 export class SendDrink extends Component {
- 
+
   state = {
     activeUsers: [],
     selectedUser: 0
   }
 
- 
-//*****************************************************************************************************
-//ComponentDidMount()
-//*****************************************************************************************************
-componentDidMount(){
-  
+
+  //*****************************************************************************************************
+  //ComponentDidMount()
+  //*****************************************************************************************************
+  componentDidMount() {
+
     const barId = localStorage.getItem("active-bar")
-  
 
-    ApiManager.get("bars",barId,"_embed=users")
-    .then((activeUsersArr)=>{
-      this.setState({
-        activeUsers: activeUsersArr.users
-          })
-    })
 
-}
+    ApiManager.get("bars", barId, "_embed=users")
+      .then((activeUsersArr) => {
+        this.setState({
+          activeUsers: activeUsersArr.users
+        })
+      })
+    ////////// NEED TO FILTER THE ARRAY FOR USERS THAT ARE NOT ME
+  }
 
 
   //*****************************************************************************************************
@@ -42,29 +42,28 @@ componentDidMount(){
           width={600}
           height={300}
           displayQuantityOfSide={0.5}
-          navigation = {false}
-          infiniteScroll = {true}
+          navigation={false}
+          infiniteScroll={true}
           enableHeading={false}
-         // active={this.state.active}
-          clickable ={true}
+          // active={this.state.active}
+          clickable={true}
         >
-            {this.state.activeUsers.map((activeUser) => { // populating the images
-                return <img key={activeUser.id} id={activeUser.id} src={activeUser.avatarUrl} alt={activeUser.tagLine} onClick={()=>{
-                  this.setState({
-                    selectedUser: activeUser.id  // setting the selected user in state
-                      })
-                }}/>  
-              })} 
+          {this.state.activeUsers.map((activeUser) => { // populating the images
+            return <img key={activeUser.id} id={activeUser.id} src={activeUser.avatarUrl} alt={activeUser.tagLine} onClick={() => {
+              this.setState({
+                selectedUser: activeUser.id  // setting the selected user in state
+              })
+            }} />
+          })}
 
-      
+
         </Coverflow>
 
-        <Button variant="contained" color="secondary" startIcon={<LocalBarOutlinedIcon/>} onClick={()=>{
+        <Button variant="contained" color="secondary" onClick={() => {
           console.log("Drink with user:", this.state.selectedUser)
-                }
-          }>
-            Drink
-          </Button>
+        }}>
+          {<LocalBarOutlinedIcon />}
+        </Button>
 
       </>
     )
