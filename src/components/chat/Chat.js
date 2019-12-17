7bf82e755bbb78a16be0c2396ae3d9b0c4f159a3
Cherkesky <guy@cherkesky.com>
2019-12-17 16:09:34 -0600

    added ascii signature

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -20,32 +20,23 @@ export class Chat extends Component {
 
     // the party is over. time to clean up the mess....
 
-    // let resetDrinkId = {}
+    let resetActiveChat = {}
 
-    // resetDrinkId = {   // resetting drinkId for the active user
-    //   id: this.state.activeUserId,
-    //   drinkId: 0
-    // }
-    // console.log("resetDrinkId", resetDrinkId)
+    resetActiveChat = {   // resetting drinkId for the active user
+      id: this.state.activeUserId,
+      activeChat: false
+    }
+    console.log("resetActiveChat", resetActiveChat)
 
-    // ApiManager.update("users", resetDrinkId) // PATCH
-      // .then(() => {
-        for  (let i=0; i<this.state.messages.length; i++){
-           ApiManager.delete("messages", this.state.messages[i].id)// looping and deleting all messages
-             console.log('Message ID:', this.state.messages[i].id )}
+    ApiManager.update("users", resetActiveChat) // PATCH
+     .then(()=>{
 
-        // localStorage.removeItem("active-chat") // remove chat from local storage
-        this.props.history.push("/senddrinks") // go back to the SendDrinks view
-      // })
-
-      // this.state.messages.map(message =>
-      //   ApiManager.delete("messages", this.state.message.id))
-      
-
-    // for  (let i=0; i<this.state.messages.length; i++){
-    //   ApiManager.delete("messages", this.state.messages[i].id)// looping and deleting all messages
-    //   console.log('Message ID:', this.state.messages[i].id )
-    // }
+      for  (let i=0; i<this.state.messages.length; i++){
+        ApiManager.delete("messages", this.state.messages[i].id)// looping and deleting all messages
+          console.log('Message ID:', this.state.messages[i].id )}
+     })
+     localStorage.removeItem("active-chat") // remove chat from local storage
+     this.props.history.push("/senddrinks") // go back to the SendDrinks view
   }
 
   //*****************************************************************************************************
