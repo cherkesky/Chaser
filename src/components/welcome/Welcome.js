367f0a6f80045ef15b8be7661c7e3d9b062eb8b0
Cherkesky <guy@cherkesky.com>
2019-12-19 12:40:15 -0600

    got background image

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
+import Background from '../../assets/bg.png';
 import Logo from '../../assets/ChaserLogo.png'
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 
+const styles = {
+  Container: {
+      backgroundImage: `url(${Background})`,
+      height: 812,
+      width: 375,
+      margin: 0,
+      padding:10,
+      backgroundPosition: "center center",
+      backgroundSize: "auto"
+      
+  }
+}
 
 export class Welcome extends Component {
   render() {
     return (
       <div>
-        <Container>
+        <Container id="Container" style={ styles.Container } >
         <img src={Logo} alt={"logo"} /> <br /><br />
 
           <Button variant="contained" color="secondary"onClick={() => {
